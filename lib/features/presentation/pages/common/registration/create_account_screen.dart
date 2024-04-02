0e51ad1377e8f4b44753cb7fbe85921f2a9c91da
SURYA <surya.pandey@ailoitte.com>
2024-04-02 18:58:07 +0530

    Add : switch user

--- lib/features/presentation/pages/common/registration/create_account_screen.dart
+++ lib/features/presentation/pages/common/registration/create_account_screen.dart
@@ -2,6 +2,7 @@ import 'package:ailoitte_components/ailoitte_components.dart';
 import 'package:flutter/material.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:my_sutra/ailoitte_component_injector.dart';
+import 'package:my_sutra/core/common_widgets/custom_button.dart';
 import 'package:my_sutra/core/common_widgets/mobile_form_widget.dart';
 import 'package:my_sutra/core/common_widgets/text_form_field_widget.dart';
 import 'package:my_sutra/core/utils/app_colors.dart';
@@ -9,6 +10,7 @@ import 'package:my_sutra/core/utils/app_decoration.dart';
 import 'package:my_sutra/core/utils/custom_inkwell.dart';
 import 'package:my_sutra/core/utils/string_keys.dart';
 import 'package:my_sutra/features/presentation/pages/common/registration/widgets/app_logo_with_terms_condition_widget.dart';
+import 'package:my_sutra/routes/routes_constants.dart';
 
 class CreateAccountScreen extends StatefulWidget {
   final String profession;
@@ -28,6 +30,7 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
   final TextEditingController _emailCtrl = TextEditingController();
   final TextEditingController _specializationCtrl = TextEditingController();
   final TextEditingController _regNumCtrl = TextEditingController();
+  final TextEditingController _socialCtrl = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -39,10 +42,28 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
             const SafeArea(child: SizedBox()),
             const AppLogoWithTermsConditionWidget(),
             const SizedBox(height: 60),
-            Text(
-              "For ${widget.profession}s",
-              style: theme.publicSansFonts
-                  .mediumStyle(fontSize: 14, fontColor: AppColors.primaryColor),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "For ${widget.profession}s",
+                  style: theme.publicSansFonts.mediumStyle(
+                      fontSize: 14, fontColor: AppColors.primaryColor),
+                ),
+                InkWell(
+                  onTap: () {
+                    AiloitteNavigation.intentWithClearAllRoutes(
+                        context, AppRoutes.chooseAccountTypeRoute);
+                  },
+                  child: Text(
+                    "Switch user",
+                    style: theme.publicSansFonts.mediumStyle(
+                        decoration: TextDecoration.underline,
+                        fontSize: 14,
+                        fontColor: AppColors.primaryColor),
+                  ),
+                ),
+              ],
             ),
             Text(
               "Create an account",
@@ -89,14 +110,43 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
               title: "Email",
               controller: _emailCtrl,
             ),
-            TextFormFieldWidget(
-              title: "Specialization",
-              controller: _specializationCtrl,
+            if (widget.profession == "Doctor") ...[
+              TextFormFieldWidget(
+                title: "Specialization",
+                controller: _specializationCtrl,
+              ),
+              TextFormFieldWidget(
+                title: "Professional Registration number",
+                controller: _regNumCtrl,
+              ),
+            ] else if (widget.profession == "Influencer")
+              TextFormFieldWidget(
+                title: "Social Profile URL",
+                controller: _socialCtrl,
+              ),
+            const SizedBox(height: 70),
+            CustomButton(
+              onPressed: () {},
+              text: "Continue",
             ),
-            TextFormFieldWidget(
-              title: "Professional Registration number",
-              controller: _regNumCtrl,
+            const SizedBox(height: 15),
+            Center(
+              child: InkWell(
+                onTap: () {
+                  AiloitteNavigation.intentWithClearAllRoutes(
+                      context, AppRoutes.loginRoute);
+                },
+                child: Text(
+                  "Sign in Instead",
+                  style: theme.publicSansFonts.regularStyle(
+                    fontSize: 16,
+                    fontColor: AppColors.primaryColor,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
             ),
+            const SizedBox(height: 40),
           ],
         ),
       ),
