122d5819eae40f2f7432be0dc678627df25ea33d
nikirana22 <nikirana1983@gmail.com>
2024-04-11 18:40:19 +0530

    refactor(en.json): added texts.

--- lib/core/utils/string_keys.dart
+++ lib/core/utils/string_keys.dart
@@ -1,7 +1,8 @@
-
 class StringKeys {
   static const String login = 'login';
+  static const String loginWithOTP = 'login_with_otp';
   static const String mobileNumber = "mobile_number";
   static const String verify = "verify";
   static const String chooseAccount = "choose_Account";
+  static const String searchResult = 'search_result';
 }
