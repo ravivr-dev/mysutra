2e232c06f07a8cb66d79665385ad02d7a70ed82f
nikirana22 <nikirana1983@gmail.com>
2024-04-26 10:01:00 +0530

    refactor(utils): added capFirstLetter method.

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -4,4 +4,10 @@ class Utils {
     final minute = minutes - (hour * 60);
     return '${'$hour'.padLeft(2, '0')}:${'$minute'.padLeft(2, '0')} ${hour < 12 ? 'AM' : 'PM'}';
   }
+
+  static String capFirstLetter(String text) {
+    return text.isNotEmpty
+        ? '${text[0]}${text.substring(1).toLowerCase()}'
+        : text;
+  }
 }
