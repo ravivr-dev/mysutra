a3324d492177d77006aebbe969dfd7f1177954cb
nikirana22 <nikirana1983@gmail.com>
2024-05-01 16:16:50 +0530

    refactor(pateint_appointment_bottom_sheet): branch merging changes.

--- lib/features/presentation/common/home/screens/bottom_sheets/patient_appointment_bottom_sheet.dart
+++ lib/features/presentation/common/home/screens/bottom_sheets/patient_appointment_bottom_sheet.dart
@@ -1,12 +1,16 @@
 import 'package:ailoitte_components/ailoitte_components.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_sutra/core/common_widgets/time_container.dart';
+import 'package:my_sutra/core/utils/utils.dart';
 import 'package:my_sutra/features/domain/entities/patient_entities/appointment_entity.dart';
 import 'package:my_sutra/routes/routes_constants.dart';
 
 import '../../../../../../ailoitte_component_injector.dart';
 import '../../../../../../core/utils/app_colors.dart';
 import '../../../../../../generated/assets.dart';
+import '../../../../../../injection_container.dart';
+import '../../cubit/home_cubit.dart';
 
 class PatientAppointmentBottomSheet extends StatelessWidget {
   final AppointmentEntity entity;
@@ -14,63 +18,87 @@ class PatientAppointmentBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.white,
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  component.text('Anika Rawat',
-                      style: theme.publicSansFonts.mediumStyle(
-                        fontSize: 16,
-                      )),
-                  Row(
-                    children: [
-                      component.assetImage(path: Assets.iconsPhone2),
-                      component.spacer(width: 2),
-                      component.text('+91 9898767656',
-                          style: theme.publicSansFonts.regularStyle(
-                            fontColor: AppColors.black81,
-                          ))
-                    ],
-                  )
-                ],
-              ),
-              TimeContainer(time: '')
-            ],
-          ),
-          component.spacer(height: 4),
-          component.text(
-            'Reason for visit here',
-            style: theme.publicSansFonts.regularStyle(
-              fontColor: AppColors.black81,
+    return BlocProvider<HomeCubit>(
+      create: (context) => sl<HomeCubit>(),
+      child: BlocConsumer<HomeCubit, HomeState>(
+        listener: (context, state) {
+          if (state is CancelAppointmentLoadedState) {
+            AiloitteNavigation.intent(context, AppRoutes.bookingCancelled);
+          }
+        },
+        builder: (context, state) {
+          return Container(
+            color: AppColors.white,
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        component.text(entity.username,
+                            style: theme.publicSansFonts.mediumStyle(
+                              fontSize: 16,
+                            )),
+                        Row(
+                          children: [
+                            component.assetImage(path: Assets.iconsPhone2),
+                            component.spacer(width: 2),
+                            component.text(
+                                '${entity.countryCode} ${entity.phoneNumber}',
+                                style: theme.publicSansFonts.regularStyle(
+                                  fontColor: AppColors.black81,
+                                ))
+                          ],
+                        )
+                      ],
+                    ),
+                    TimeContainer(
+                        time:
+                            '${Utils.getMonthDay(entity.date)}, ${entity.time}')
+                  ],
+                ),
+                component.spacer(height: 4),
+                component.text(
+                  entity.reason,
+                  style: theme.publicSansFonts.regularStyle(
+                    fontColor: AppColors.black81,
+                  ),
+                ),
+                component.spacer(height: 36),
+                _buildItems(callback: () {}, text: 'Join Video Consultation'),
+                _buildDivider(),
+                _buildItems(
+                    callback: () => AiloitteNavigation.intentWithData(
+                        context, AppRoutes.chatScreen, entity),
+                    text: 'Send Message'),
+                _buildDivider(),
+                _buildItems(callback: () {}, text: 'View patient history'),
+                _buildDivider(),
+                _buildItems(
+                    callback: () {
+                      AiloitteNavigation.intentWithData(
+                          context, AppRoutes.rescheduleAppointment, entity.id);
+                    },
+                    text: 'Reschedule Appointment'),
+                _buildDivider(),
+                _buildItems(
+                    callback: () {
+                      context
+                          .read<HomeCubit>()
+                          .cancelAppointmentForDoctor(appointmentId: entity.id);
+                    },
+                    text: 'Cancel Appointment',
+                    textColor: AppColors.color0xFFFF1100),
+              ],
             ),
-          ),
-          component.spacer(height: 36),
-          _buildItems(callback: () {}, text: 'Join Video Consultation'),
-          _buildDivider(),
-          _buildItems(
-              callback: () => AiloitteNavigation.intentWithData(
-                  context, AppRoutes.chatScreen, entity),
-              text: 'Send Message'),
-          _buildDivider(),
-          _buildItems(callback: () {}, text: 'View patient history'),
-          _buildDivider(),
-          _buildItems(callback: () {}, text: 'Reschedule Appointment'),
-          _buildDivider(),
-          _buildItems(
-              callback: () {},
-              text: 'Cancel Appointment',
-              textColor: AppColors.color0xFFFF1100),
-        ],
+          );
+        },
       ),
     );
   }
