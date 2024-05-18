beaa4ac7949b02f0b717aa174fd31ca0bac0ceb4
abnv-ailoitte <abhinav.kumar@ailoitte.com>
2024-05-19 02:16:29 +0530

    refactor(posts_model): moved post_id_model to separate file

--- lib/features/data/model/post_models/posts_model.dart
+++ lib/features/data/model/post_models/posts_model.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:my_sutra/features/data/model/post_models/media_url_model.dart';
+import 'package:my_sutra/features/data/model/post_models/post_id_model.dart';
 import 'package:my_sutra/features/data/model/post_models/post_user_model.dart';
 
 class PostModel {
@@ -113,78 +114,78 @@ class PostData {
       };
 }
 
-class PostIdModel {
-  final String id;
-  final PostUserModel userId;
-  final bool isFollowing;
-  final String content;
-  final List<MediaUrlModel> mediaUrls;
-  final List<String> taggedUserIds;
-  final int totalLikes;
-  final int totalComments;
-  final int totalShares;
-  final int repostCount;
-  final bool isMyPost;
-  final bool isLiked;
-  final bool isRepostedByMe;
-  final DateTime createdAt;
-  final DateTime updatedAt;
 
-  PostIdModel({
-    required this.id,
-    required this.userId,
-    required this.isFollowing,
-    required this.content,
-    required this.mediaUrls,
-    required this.taggedUserIds,
-    required this.totalLikes,
-    required this.totalComments,
-    required this.totalShares,
-    required this.repostCount,
-    required this.isMyPost,
-    required this.isLiked,
-    required this.isRepostedByMe,
-    required this.createdAt,
-    required this.updatedAt,
-  });
 
-  factory PostIdModel.fromRawJson(String str) =>
-      PostIdModel.fromJson(json.decode(str));
 
-  String toRawJson() => json.encode(toJson());
 
-  factory PostIdModel.fromJson(Map<String, dynamic> json) => PostIdModel(
-        id: json["_id"],
-        userId: PostUserModel.fromJson(json["userId"]),
-        isFollowing: json["isFollowing"],
-        content: json["content"],
-    mediaUrls: List<MediaUrlModel>.from(
-        json["mediaUrls"].map((x) => MediaUrlModel.fromJson(x))),
-    taggedUserIds: List<String>.from(json["taggedUserIds"].map((x) => x)),
-        totalLikes: json["totalLikes"],
-        totalComments: json["totalComments"],
-        totalShares: json["totalShares"],
-        repostCount: json["repostCount"],
-        isMyPost: json["isMyPost"],
-        isLiked: json["isLiked"],
-        isRepostedByMe: json["isRepostedByMe"],
-        createdAt: DateTime.parse(json["createdAt"]),
-        updatedAt: DateTime.parse(json["updatedAt"]),
-      );
 
-  Map<String, dynamic> toJson() => {
-        "_id": id,
-        "userId": userId.toJson(),
-        "isFollowing": isFollowing,
-        "content": content,
-        "mediaUrls": List<dynamic>.from(mediaUrls.map((x) => x)),
-        "taggedUserIds": List<dynamic>.from(taggedUserIds.map((x) => x)),
-        "totalLikes": totalLikes,
-        "totalComments": totalComments,
-        "totalShares": totalShares,
-        "repostCount": repostCount,
-        "isMyPost": isMyPost,
-        "isLiked": isLiked,
-        "isRepostedByMe": isRepostedByMe,
-      };
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
