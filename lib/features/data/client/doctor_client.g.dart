fb0249dfc2c8d1d46a9ec7938bc855df08fbd743
surya <surya.pandey@ailoitte.com>
2024-05-31 12:04:20 +0530

    Add: create UPI API

--- lib/features/data/client/doctor_client.g.dart
+++ lib/features/data/client/doctor_client.g.dart
@@ -328,6 +328,32 @@ class _DoctorClient implements DoctorClient {
     return value;
   }
 
+  @override
+  Future<dynamic> createUpi(String upi) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = {'vpa': upi};
+    final _result = await _dio.fetch(_setStreamType<dynamic>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/payment/payout/fund-account/vpa',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        ))));
+    final value = _result.data;
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
