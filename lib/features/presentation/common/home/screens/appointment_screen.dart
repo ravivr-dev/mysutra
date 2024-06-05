802138e7a1f861f59fd98e8142ab4406fd380789
abnv-ailoitte <abhinav.kumar@ailoitte.com>
2024-06-05 17:53:59 +0530

    added empty list check

--- lib/features/presentation/common/home/screens/appointment_screen.dart
+++ lib/features/presentation/common/home/screens/appointment_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ailoitte_components/ailoitte_components.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
